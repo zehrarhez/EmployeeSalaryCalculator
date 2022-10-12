c3a7f17955589e98a54bdbbca7cd87bd414c00f5
zehrarhez <86465805+zehrarhez@users.noreply.github.com>
2022-10-13 02:30:40 +0300

    Update EBebekFirstCase.java

--- EBebekFirstCase.java
+++ EBebekFirstCase.java
@@ -2,10 +2,12 @@ package ebebekfirstcase;
 public class EBebekFirstCase {
 
     public static void main(String[] args) {
-        Employee emp1 = new Employee("Zehra Özeren" , 8000 , 47 , 2020);
-        Employee emp2 = new Employee("Ünal Erdoğan" , 9000, 45, 2019);
+        Employee emp1 = new Employee("Zehra Özeren" , 8000 , 47 , 2006);
+        Employee emp2 = new Employee("Ünal Erdoğan" , 9000, 45, 2000);
+        Employee emp3 = new Employee("Ayça Çelik" , 750 , 25, 2021);
         emp1.toString(emp1);
         emp2.toString(emp2);
+        emp3.toString(emp3);
     
     
     
